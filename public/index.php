aa2aa1d3f55b55b1a6c3460e036c4595451b3a12
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-08-26 16:10:56 +0545

    connection successfully made

--- public/index.php
+++ public/index.php
@@ -4,12 +4,19 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 //use Slim\Factory\AppFactory;
 
 require '../vendor/autoload.php';
+require '../includes/DbConnect.php';
 
 $app = new \Slim\App;
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
+
+    $db = new DbConnect;
+    if($db->connect() != null){
+        echo '    Connection Successful';
+    }
+
     return $response;
 });
 
