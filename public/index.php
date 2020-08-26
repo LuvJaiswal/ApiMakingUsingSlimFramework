ad0b18a32f4d3c4ab154b11f6c66f675515ff364
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-08-26 14:20:50 +0545

    api project successfully setup

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-require __DIR__ . '/../vendor/autoload.php';
-$app = AppFactory::create();
+//use Slim\Factory\AppFactory;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
 });
+
 $app->run();
